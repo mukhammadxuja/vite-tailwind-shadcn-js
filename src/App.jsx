c6d856ece761f00d3ffb5c37a95fd33c4eaa4573
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2023-11-04 17:26:11 +0500

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import reactLogo from '@/assets/react.svg';
+import { ThemeProvider } from '@/provider/ThemeProvider';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { ThemeToggle } from '@/components/ThemeToggle';
 
 import './App.css';
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-600">
-        Tailwind css working!
-      </h1>
-      <div className='flex gap-2 mt-2'>
-        <Button>Button</Button>
-        <Input placeholder="Search" />
-      </div>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <h1 className="text-3xl font-bold text-slate-900 dark:text-white">
+          Tailwind css working!
+        </h1>
+        <div className="flex gap-2 mt-2">
+          <Button>Button</Button>
+          <Input placeholder="Search" />
+          <ThemeToggle />
+        </div>
+      </ThemeProvider>
     </>
   );
 }
