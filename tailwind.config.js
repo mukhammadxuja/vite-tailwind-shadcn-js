3a5fb2e46aa17e1ca079599afaea71a2142fcd52
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2025-01-05 17:01:29 +0500

    fix: update tailwind configuration to use ES module syntax for plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 export default {
   darkMode: ['class'],
   content: [
@@ -131,5 +133,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [tailwindcssAnimate],
 };
